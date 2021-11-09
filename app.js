b6c21bcdb5f295f42138d199b58c9739abd124ba
Andretreccia <andreabtreccia@gmail.com>
2021-11-09 17:59:55 +0100

    Stampa a schermo della lista email

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const app = new Vue ({
     el: '#app',
     data:{
-    
+        listaEmail:[
+
+        ],
+    },
+    methods:{
+        richiestaPsw(){
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(risposta => {
+                this.listaEmail.push(risposta.data.response);
+                console.log(risposta.data.response)
+            })
+        }
     },
     mounted(){
-        
+        for (let i=0; i<10; i++) {
+            this.richiestaPsw();
+        }
     }
 })
